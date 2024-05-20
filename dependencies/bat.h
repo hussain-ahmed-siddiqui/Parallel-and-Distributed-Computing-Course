e43ba045f737683b7b719d223fbd93651f578534
hussain-ahmed-siddiqui <hussain.siddiqui.5th@gmail.com>
2024-05-20 17:04:04 +0500

    done
    hell yeah

--- dependencies/bat.h
+++ dependencies/bat.h
@@ -1,16 +1,16 @@
 
 #define ALPHA 1.5
 #define BETA 2
-#define GAMMA 1.7
+#define GAMMA 1.5
 #define k 1
 #define c 0.01
 #define r 0.05
 #define VELOCITY_UPPER_BOUND   10
-#define VELOCITY_LOWER_BOUND  -10
+#define VELOCITY_LOWER_BOUND  -5
 #define POSITION_UPPER_BOUND   20
-#define POSITION_LOWER_BOUND  -20
-#define LOUDNESS_CONSTANT 0.9
-#define PULSE_RATE_CONSTANT 1.1
+#define POSITION_LOWER_BOUND  -200
+#define LOUDNESS_CONSTANT 0.95
+#define PULSE_RATE_CONSTANT 0.1
 using namespace std;
 
    auto now = chrono::high_resolution_clock::now();
@@ -29,7 +29,7 @@ using namespace std;
 
     mt19937 gen(seed); // Seed the generator with current time
 
-    double getRandomNumber(int lower_limit, int upper_limit){
+    float getRandomNumber(int lower_limit, int upper_limit){
         uniform_real_distribution<> randum(lower_limit, upper_limit); // define the range
         return randum(gen);
     }
@@ -49,12 +49,12 @@ struct Bat{
         velocity = getRandomNumber(-1,1);
         frequency = getRandomNumber(0,1);
         loudness = 2;
-        pulse_rate = 0.1;
+        pulse_rate = 0.2;
         initial_pulse_rate = pulse_rate;
     }
 
     void evaluateFitness(){
-        fitness = (ALPHA*k*position)/(1+BETA*c*pow(position,2)) + (GAMMA*r*pow(velocity,2));
+        fitness = ((float)ALPHA*k*position)/((float)1+(float)BETA*c*pow(position,2)) + ((float)GAMMA*r*pow(velocity,2));
         if(fitness > personal_best_fitness){
             personal_best_fitness = fitness;
             personal_best_position = position;
--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include"dependencies/libs.h"
 #include"dependencies/bat.h"
 
-float global_best_fitness=FLT_MIN;
+float global_best_fitness=-FLT_MIN;
 float average_best_position_of_batSwarm;
-float global_best_position=FLT_MIN;
+float global_best_position=-FLT_MIN;
 void updateBatFrequency(float &freq){
     freq = getRandomNumber(0,1); //simplified from fmin + (fmax - fmin) * randomNumber
+    if(freq > 0.1 && getRandomNumber(0,1) > 0.5){
+        freq*=getRandomNumber(0,1);
+    }
 }
 
 void updateBatVelocity(float &current_v, float &current_p, float &freq, float lower_limit, float upper_limit){
@@ -14,8 +17,20 @@ void updateBatVelocity(float &current_v, float &current_p, float &freq, float lo
     else if(current_v < lower_limit) current_v = lower_limit;
 }
 
-void updateBatPosition(float &current_p, float &new_v, float lower_limit, float upper_limit){
-    current_p += new_v;
+void updateBatPulseRate(float &current_pr, float &initial_pr){
+    current_pr = initial_pr* (1 - exp(-PULSE_RATE_CONSTANT * GAMMA));
+}
+
+void updateBatPosition(float &current_p, float &new_v, float &current_pr,float &initial_pr, float lower_limit, float upper_limit){
+    float prev_pr = current_pr;
+    updateBatPulseRate(current_pr, initial_pr);
+    if (current_pr < prev_pr ) {  // Assuming rand() returns a float between 0 and 1
+        // Perform a local search towards the global best position
+        current_p += (global_best_position - current_p) * 0.01; // Example: move halfway towards global best
+    } else {
+        // Regular position update
+        current_p += new_v;
+    }
     if(current_p > upper_limit) current_p = upper_limit;
     else if(current_p < lower_limit) current_p = lower_limit;
 }
@@ -24,16 +39,12 @@ void updateBatLoudness(float &current_l){
     current_l *= LOUDNESS_CONSTANT;
 }
 
-void updateBatPulseRate(float &current_pr, float &initial_pr){
-    current_pr = initial_pr* (1 - exp(-PULSE_RATE_CONSTANT * GAMMA));
-}
 
 int avg_personal_best_position_improv_counter=0;
-bool ApplyStoppingCriteria(float new_avg, float prev_avg){
+bool ApplyStoppingCriteria(float &prev_avg, float new_avg){
     avg_personal_best_position_improv_counter++;
-    // if(new_avg < 0 && prev_avg > 0)
-    // if(new_avg > prev_avg){avg_personal_best_position_improv_counter=0;}
-    if(avg_personal_best_position_improv_counter > 10) return true;
+    if(fabs(new_avg) > fabs(prev_avg)){avg_personal_best_position_improv_counter=0;}
+    if(avg_personal_best_position_improv_counter > 6) return true;
     return false;
 
 }
@@ -65,7 +76,7 @@ void startAlgo(vector<Bat> bats){
         for(int i=0;i<bats.size();i++){
             updateBatFrequency(bats[i].frequency);
             updateBatVelocity(bats[i].velocity,bats[i].position, bats[i].frequency,VELOCITY_LOWER_BOUND,VELOCITY_UPPER_BOUND);
-            updateBatPosition(bats[i].position,bats[i].velocity,POSITION_LOWER_BOUND,POSITION_UPPER_BOUND);
+            updateBatPosition(bats[i].position,bats[i].velocity,bats[i].pulse_rate,bats[i].initial_pulse_rate,POSITION_LOWER_BOUND,POSITION_UPPER_BOUND);
             updateBatLoudness(bats[i].loudness);
             updateBatPulseRate(bats[i].pulse_rate,bats[i].initial_pulse_rate);
             bats[i].evaluateFitness();
@@ -76,7 +87,8 @@ void startAlgo(vector<Bat> bats){
             }
             
         }
-    if(ApplyStoppingCriteria(CalculateFitnessAverage(bats),average_best_position_of_batSwarm)) break;
+        float prev_avg = average_best_position_of_batSwarm;
+    if(ApplyStoppingCriteria(prev_avg,CalculateFitnessAverage(bats))) break;
     cout<<"Global fitness: "<<global_best_fitness<<" Average position: " <<average_best_position_of_batSwarm<<"\n";
     
     }
@@ -85,6 +97,6 @@ void startAlgo(vector<Bat> bats){
 }
 
 int main(){
-    vector<Bat> bats(10);
+    vector<Bat> bats(50);
     startAlgo(bats);
 }
